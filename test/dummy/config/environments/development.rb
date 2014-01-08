351a2b3c289ef46a0a2ef69e334618f0733ce9e5
Will Tomlins <will@tomlins.org.uk>
2014-01-08 10:58:44 +0000

    Updated to use rails 4

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -1,6 +1,6 @@
 Dummy::Application.configure do
+  config.eager_load = false
   config.cache_classes = false
-  config.whiny_nils = true
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.active_support.deprecation = :log
--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -1,8 +1,8 @@
 Dummy::Application.configure do
+  config.eager_load = false
   config.cache_classes = true
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
-  config.whiny_nils = true
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.action_dispatch.show_exceptions = false
